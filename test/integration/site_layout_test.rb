b34309f2b5e01b0b8ea6d99629a4bcaff2645058
landsurveyor450 <landsurveyor450@gmail.com>
2019-03-07 08:54:09 +0300

    add test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "layout_links" do
+    get root_path
+    assert_select "a[href=?]", about_path
+  end
+end
